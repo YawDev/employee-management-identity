1d41dd48107eb30612e3d742596b840cc25e397b
YawDev <jason.ampah.dev@gmail.com>
2025-11-04 08:28:16 -0500

    TODO for later

--- employee.management.identity.core/Business/TokenService.cs
+++ employee.management.identity.core/Business/TokenService.cs
@@ -4,5 +4,7 @@ namespace employee.management.identity.core.Business
     {
         //TODO: Implement JWT token generation methods here
         // Better alternative than cookie based sessions.
+        // Consider using libraries like System.IdentityModel.Tokens.Jwt or Microsoft.IdentityModel.Tokens
+        // Send access and ID tokens to clients after successful authentication
     }
 }
